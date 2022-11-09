2e843314f581f969c779b7aaf9a9e393b67aa87a
whrod <whrodtube@gmail.com>
2022-11-09 19:09:33 +0900

    Fix : typo correction

--- server/src/mqtt/mqtt.controller.ts
+++ server/src/mqtt/mqtt.controller.ts
@@ -11,11 +11,11 @@ export class MqttController {
   @MessagePattern('musma_channel')
   getMusma(@Payload() data, @Ctx() context: MqttContext) {
     console.log('Client : getMusma, ', `Topic : ${context.getTopic()}`, data);
-    return `Client data sended: ${data}`;
+    return `Client data sent: ${data}`;
   }
   @MessagePattern('jayce_channel')
   getJayce(@Payload() data, @Ctx() context: MqttContext) {
     console.log('Client : getJayce, ', `Topic : ${context.getTopic()}`, data);
-    return `Client data sended: ${data}`;
+    return `Client data sent: ${data}`;
   }
 }
